1d7afe6484a8510016683721bc608f6c9f5f7482
Anthony Jackman <energeticpixels@gmail.com>
2022-12-11 11:04:58 -0600

    FEAT Add code coverage instrumentation
    instrumented local only.
    project will need to decide on where reporting should be hosted.
    
    Resolves #25

--- test/healthcheck.spec.js
+++ test/healthcheck.spec.js
@@ -1,8 +1,10 @@
-import { test } from 'tap';
+import test from 'tape';
 import { build } from '../server/app.js';
 
 test('TEST the /healthcheck route', async t => {
   const fastify = build();
+
+  t.plan(2);
 
   t.teardown(() => {
     fastify.close();
@@ -13,10 +15,6 @@ test('TEST the /healthcheck route', async t => {
     url: '/healthcheck'
   });
 
-  fastify.log.info(response);
-
   t.equal(response.statusCode, 200, 'StatusCode Check');
   t.same(response.json(), { say: 'Pong. I am here' }, 'message response check');
-
-  t.end();
 });
